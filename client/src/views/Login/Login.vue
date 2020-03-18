14e98d6b3ae691690287bd61c0ca8bc9b4587894
Taran Jennison <tmj49@uclive.ac.nz>
2020-03-18 15:45:19 +1300

    Alert banner for errors on login #story[210]

--- client/src/views/Login/Login.vue
+++ client/src/views/Login/Login.vue
@@ -1,5 +1,11 @@
 <template>
     <div id="app">
+        <div class="alert alert-danger alert-dismissible fade show sticky-top" role="alert" hidden="true" id="alert">
+            {{  message  }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
         <!-- Full Page Image Header with Vertically Centered Content -->
         <h1><br/></h1>
@@ -22,18 +28,17 @@
             <div class="form-group">
                 <label for="email">Email Address: </label>
                 <input type="email" class="form-control" v-model="email" id="email" placeholder="Email Address" required><br/>
-            <div class="form-group">
-                <label for="password">Password: </label>
-                <input type="password" class="form-control" v-model="password" id="password" placeholder="Password" required> <br/>
-            </div>
-            <div class="form-group">
-                <input v-on:submit="login" class="btn btn-primary" type="submit" value="Sign In">
-                <router-link to="/register" class="btn btn-link">Register</router-link>
+                <div class="form-group">
+                    <label for="password">Password: </label>
+                    <input type="password" class="form-control" v-model="password" id="password" placeholder="Password" required> <br/>
+                </div>
+                <div class="form-group">
+                    <input v-on:submit="login" class="btn btn-primary" type="submit" value="Sign In">
+                    <router-link to="/register" class="btn btn-link">Register</router-link>
 
-            </div>
+                </div>
             </div>
         </form>
-        <label v-show="hasError" id="error">Error</label>
     </div>
 </template>
 
@@ -48,7 +53,8 @@
                 email: '',
                 password: '',
                 hasError: false,
-                loggedIn: false
+                loggedIn: false,
+                message: ""
             }
         },
         methods: {
@@ -60,23 +66,26 @@
                 };
                 //Perform password encryption
                 server.post('http:/localhost:9499/login', userLogin,
-                ).then(function (user) {
-                        console.log(user);
+                ).then(function () {
                         console.log('User Logged In Successfully!');
                         this.loggedIn = true;
                     }
                 ).catch(error => {
                     this.hasError = true;
                     console.log(error.response);
-                    // let errorLabel = document.getElementById("error");
-                    // if (error.response.status == 403) {
-                    //     errorLabel.textContent = error.response.data.toString();
-                    // } else if (error.response.status == 400) {
-                    //     errorLabel.textContent = "An invalid login request has been received please try again"
-                    // } else {
-                    //     errorLabel.textContent = "An unknown error has occurred during login"
-                    // }
-
+                    let errorAlert = document.getElementById("alert");
+                    if (error.response.status === 401) {
+                        this.message = error.response.data.toString();
+                    } else if (error.response.status === 400) {
+                        this.message = "An invalid login request has been received please try again"
+                    } else {
+                        this.message = "An unknown error has occurred during login"
+                    }
+                    this.hasError = true;
+                    errorAlert.hidden = false;
+                    setTimeout(function () {
+                        errorAlert.hidden = true;
+                    }, 5000);
                 });
                 if (this.loggedIn && !this.hasError) {
                     this.$router.push("/profile");
