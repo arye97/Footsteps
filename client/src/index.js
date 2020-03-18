2d1432889080b690b9c13a382acfe3bf09678369
Saman Ighani <sig16@uclive.ac.nz>
2020-03-18 14:26:53 +1300

    First instance of edit emails gui.
    Has a basic outline with no actual elements yet also added the path to the index so the client can navigate to the page. Mainly to make sure its running clientside, hasn't been fully implemented into to the client can only manually access the page. #implement #story[212]

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from './views/Login/Login.vue'
 import Register from './views/Register/Register.vue'
 import Home from './views/Home/Home.vue'
+import EditEmail from "./views/EditEmails/EditEmail";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Details from "./views/Settings/Details";
 
@@ -15,6 +16,7 @@ export default new Router({
   {path: '/', component: Home},
   {path: '/login', component: Login},
   {path: '/register', component: Register},
+    {path: '/editemail', component: EditEmail},
   {path: "/profile/details", component: Details},
 
   // otherwise redirect to home
