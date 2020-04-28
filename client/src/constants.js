545f676fc99608ba1bd257c2df10e79da7f9f9d4
Rebekah McKinnon <rmc209@uclive.ac.nz>
2020-04-29 11:34:39 +1200

    Updated register so that it uses values for fitness levels from a list found in constants.js and now the values saved in the DB are between the correct values. NB: Have not yet fixed the display issue on the home page #story[243]

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,3 +3,10 @@ const restCountriesBaseUrl = "https://restcountries.eu/rest/v2"
 
 // API endpoints
 export const getCountryNames = restCountriesBaseUrl + "/all?fields=name"
+
+// Fitness levels
+export const fitnessLevels = [{value: 0 , desc: "Unfit, no regular exercise, being active is very rare"},
+    {value: 1, desc: "Not overly fit, occasional recreational fitness activity, active a few times a month"},
+    {value: 2, desc: "Moderately fit, enjoys fitness activities for recreation, active once or twice a week"},
+    {value: 3, desc: "Fit, may compete occasionally in small scale events, active most days"},
+    {value: 4, desc: "Very fit, competitive athlete, extremely active"}]
