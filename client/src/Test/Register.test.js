24dc3c547fec9ad5ae2a8e0a713a8f9288931a17
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-04-30 16:05:23 +1200

    Added test for Story1 AC7.  #story[241]

--- client/src/Test/Register.test.js
+++ client/src/Test/Register.test.js
@@ -25,9 +25,28 @@ const mandatoryFieldIds = [
     ['date_of_birth-label']
 ];
 // Iterate though all mandatory field names (using their id attribute) and check that they contain an asterisk
-each(mandatoryFieldIds).test('AC1 %s is marked as a mandatory attribute (with an asterisk)', (field_name) => {
+each(mandatoryFieldIds).test('AC4 %s is marked as a mandatory attribute (with an asterisk)', (field_name) => {
     // Checks that each <label> tag above all mandatory fields ends in an asterisk.
-    expect(registerWrapper.find('#' + field_name).text()).toContain("*");
+    expect(registerWrapper.get('#' + field_name).text()).toContain("*");
 });
+
+
+// ----AC7----
+test('AC7 Gender dropdown menu contains “male”, “female”, and “non-binary”', () => {
+    // Tests that the genders array contains the right strings.
+    // Would be nice to test whether the genders array is used in the dropdown menu
+    const expected = [
+        expect.stringMatching(/male/i),   // Regular expressions to match values in the genders array
+        expect.stringMatching(/female/i),
+        expect.stringMatching(/non-binary/i),   // Could change this to accept underscore or hyphen
+    ];
+
+    expect(Register.data().genders).toEqual( expect.arrayContaining(expected) );
+    expect(Register.data().genders).toHaveLength(3);
+});
+
+
+
+
 
 
