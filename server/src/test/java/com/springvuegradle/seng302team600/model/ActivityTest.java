5e96be074064158393421130787545f385f52567
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-07-03 18:24:22 +1200

    Added blank test to Activity.  Nothing to test yet.  #story[326]

---
+++ server/src/test/java/com/springvuegradle/seng302team600/model/ActivityTest.java
@@ -0,0 +1,15 @@
+package com.springvuegradle.seng302team600.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class ActivityTest {
+
+    private Activity activityTest;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    
+}
