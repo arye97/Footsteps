44ffc6fc1582d3c6ab421b30669ad057dd9f3ec6
Euan Widjaja <ewi45@uclive.ac.nz>
2020-07-07 15:35:09 +1200

    Implement front-end attribute validation for creating activities (except for endTime and location). Also add wordcount for description and specified some validation rules. #story[326]

--- client/src/views/CreateActivity.vue
+++ client/src/views/CreateActivity.vue
@@ -17,7 +17,7 @@
             </div>
         </header>
         <div>
-            <b-form @submit="onSubmit" v-if="show">
+            <b-form @submit="onSubmit">
                 <b-form-group
                         id="input-group-1"
                         label="Name of activity: *"
@@ -26,23 +26,34 @@
                     <b-form-input
                             id="input-1"
                             v-model="activityName"
-                            required
+                            trim
                             placeholder="Your Activity Name..."
                     ></b-form-input>
                 </b-form-group>
+                <div class="alert alert-danger alert-dismissible fade show" hidden role="alert" id="alert_activity_name">
+                    Field is mandatory
+                </div>
 
                 <b-form-group
                         id="input-group-2"
                         label="Description: *"
                         label-for="input-2"
                 >
-                    <b-form-input
+                    <b-form-textarea
                             id="input-2"
+                            rows="5"
+                            trim
+                            v-on:input="updateWordCount"
                             v-model="description"
-                            required
                             placeholder="Description of your activity..."
-                    ></b-form-input>
+                    ></b-form-textarea>
+                    <div id="word-count">
+                        {{ charCount }}/{{ maxCharCount }} characters remaining
+                    </div>
                 </b-form-group>
+                <div class="alert alert-danger alert-dismissible fade show" hidden role="alert" id="alert_description">
+                    Field is mandatory and can only contain a maximum of {{ maxCharCount }} characters
+                </div>
 
                 <b-form-group
                         id="input-group-3"
@@ -55,6 +66,9 @@
                         <template slot="noResult">Invalid activity type</template>
                     </multiselect>
                 </b-form-group>
+                <div class="alert alert-danger alert-dismissible fade show" hidden role="alert" id="alert_activity_types">
+                    Field is mandatory and must have at least one activity type
+                </div>
 
                 <b-form-group
                         id="input-group-4"
@@ -72,15 +86,25 @@
                 </b-form-group>
 
 
-                <b-form-group v-if="continuous === false">
-                    <label id="input-start-time=label" for="input-start-time">Start Time: *</label>
-                    <input type="datetime-local" class="form-control" v-model="startTime" id="input-start-time">
-                </b-form-group>
+                <div v-if="continuous === false">
+                    <b-form-group>
+                        <label id="input-start-time=label" for="input-start-time">Start Time: *</label>
+                        <input type="datetime-local" class="form-control" v-model="startTime" id="input-start-time">
+                    </b-form-group>
+                    <div class="alert alert-danger alert-dismissible fade show" hidden role="alert" id="alert_start">
+                        Field is mandatory, a start date must be set with (optionally) a start time.
+                    </div>
+                </div>
 
-                <b-form-group v-if="continuous === false">
-                    <label id="input-end-time=label" for="input-end-time">End Time: *</label>
-                    <input type="datetime-local" class="form-control" v-model="endTime" id="input-end-time">
-                </b-form-group>
+                <div v-if="continuous === false">
+                    <b-form-group>
+                        <label id="input-end-time=label" for="input-end-time">End Time: *</label>
+                        <input type="datetime-local" class="form-control" v-model="endTime" id="input-end-time">
+                    </b-form-group>
+                    <div class="alert alert-danger alert-dismissible fade show" hidden role="alert" id="alert_end">
+                        Field is mandatory, an end date must be set with (optionally) an end time.
+                    </div>
+                </div>
 
                 <b-form-group
                         id="input-group-location"
@@ -90,14 +114,15 @@
                     <b-form-input
                             id="input-location"
                             v-model="location"
-                            required
                             placeholder="Location of your activity..."
                     ></b-form-input>
                 </b-form-group>
+                <div class="alert alert-danger alert-dismissible fade show" hidden role="alert" id="alert_location">
+                    Field is mandatory and a location must be set
+                </div>
                 <div>
-                    <b-button class="float-left">Back</b-button>
-                    <b-button class="float-right" type="submit" variant="primary">Submit</b-button>
-                    <!--                <b-button class="float-right" type="reset" variant="danger">Reset</b-button>-->
+                    <b-button>Back</b-button>
+                    <b-button class="float-right" type="submit" variant="primary" @submit="onSubmit">Submit</b-button>
                 </div>
             </b-form>
             <footer class="col-12 text-center">
@@ -112,13 +137,20 @@
     import Header from '../components/Header/Header.vue'
     import server from "../Api";
 
+    function showError(alert_name) {
+        let errorAlert = document.getElementById(alert_name);
+
+        errorAlert.hidden = false;          //Show alert bar
+        setTimeout(function () {    //Hide alert bar after ~9000ms
+            errorAlert.hidden = true;
+        }, 9000);
+    }
+
     export default {
         components: { Header, Multiselect },
         name: "CreateActivity",
         data() {
             return {
-                show: true,
-
                 profileId: null,
                 activityName: null,
                 description: null,
@@ -127,27 +159,60 @@
                 continuous: true,
                 startTime: null,
                 endTime: null,
-                location: null
+                location: null,
+
+                charCount: 0,
+                maxCharCount: 280
             }
         },
         mounted() {
+            if (!sessionStorage.getItem("token")) {
+                this.$router.push('/login'); //Routes to home on logout
+            }
             this.fetchActivityTypes();
-            // if (!sessionStorage.getItem("token")) {
-            //     this.$router.push('/login'); //Routes to home on logout
-            // } else {
-            //
-            // }
         },
         methods: {
             onSubmit(evt) {
                 evt.preventDefault();
-
+                this.validateActivityInputs();
             },
+
+            validateActivityInputs() {
+                if (!this.activityName) {
+                    showError('alert_activity_name');
+                }
+
+                if (!this.description || this.description.length > this.maxCharCount) {
+                    showError('alert_description');
+                }
+
+                if (this.selectedActivityTypes.length < 1) {
+                    showError('alert_activity_types');
+                }
+
+                if (!this.continuous) {
+                    if (!this.startTime) {
+                        console.log(this.startTime);
+                        showError('alert_start');
+                    } // penus
+                    if (!this.endTime) {
+                        console.log(this.endTime);
+                        showError('alert_end');
+                    } else if (this.endTime > this.startTime) {
+
+                    }
+                }
+            },
+
+            updateWordCount() {
+                console.log(this.description);
+                this.charCount = this.description.length;
+            },
+
             /**
              * Fetch all possible activity types from the server
              */
             async fetchActivityTypes() {
-                this.activityTypes = null;
                 await server.get('activity-types',
                     {headers: {'Content-Type': 'application/json', 'Token': sessionStorage.getItem("token")}
                     }
@@ -168,5 +233,11 @@
 <style scoped>
     footer {
         padding-top: 55px;
+    }
+
+    #word-count {
+        padding-top: 7px;
+        color: #707070;
+        font-size: 0.8em;
     }
 </style>
