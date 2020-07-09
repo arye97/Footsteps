6fc0a9d39761839378565b95fa3c35538af9492a
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-07-09 13:05:39 +1200

    Added testing for ActivityTypeService.  #story[326]

--- server/src/main/java/com/springvuegradle/seng302team600/service/ActivityTypeService.java
+++ server/src/main/java/com/springvuegradle/seng302team600/service/ActivityTypeService.java
@@ -1,26 +1,22 @@
 package com.springvuegradle.seng302team600.service;
 
 import com.springvuegradle.seng302team600.model.ActivityType;
-import com.springvuegradle.seng302team600.model.Email;
-import com.springvuegradle.seng302team600.model.User;
-import com.springvuegradle.seng302team600.payload.UserResponse;
 import com.springvuegradle.seng302team600.repository.ActivityTypeRepository;
-import com.springvuegradle.seng302team600.repository.EmailRepository;
-import com.springvuegradle.seng302team600.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.security.SecureRandom;
 import java.util.HashSet;
 import java.util.Set;
 
 @Service("activityTypePersistService")
 public class ActivityTypeService {
 
-    @Autowired
     private ActivityTypeRepository activityTypeRepository;
+
+    public ActivityTypeService(ActivityTypeRepository activityTypeRepository) {
+        this.activityTypeRepository = activityTypeRepository;
+    }
 
     /**
      * Takes a Set<ActivityType> of entities not in the database, finds their equivalents in
--- server/src/test/java/com/springvuegradle/seng302team600/service/ActivityTypeServiceTest.java
+++ server/src/test/java/com/springvuegradle/seng302team600/service/ActivityTypeServiceTest.java
@@ -1,42 +1,31 @@
-package com.springvuegradle.seng302team600.controller;
+package com.springvuegradle.seng302team600.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springvuegradle.seng302team600.model.ActivityType;
 import com.springvuegradle.seng302team600.repository.ActivityTypeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ActivityTypeController.class)
-class ActivityTypeControllerTest {
+@WebMvcTest(ActivityTypeService.class)
+class ActivityTypeServiceTest {
 
     @MockBean
     private ActivityTypeRepository activityTypeRepository;
-    @Autowired
-    private MockMvc mvc;
 
+    private ActivityTypeService activityTypeService;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
-    private final String validToken = "valid";
     private List<ActivityType> activityTypeMockTable = new ArrayList<>();
     private static final Long DEFAULT_ACTIVITY_TYPE_ID = 1L;
     private static Long activityTypeCount = 0L;
@@ -49,7 +38,6 @@ class ActivityTypeControllerTest {
         activityTypeCount = 0L;
         // Save
         when(activityTypeRepository.save(Mockito.any(ActivityType.class))).thenAnswer(i -> {
-//            ReflectionTestUtils.setField(newActivity, "activityId", DEFAULT_ACTIVITY_TYPE_ID + activityTypeCount++);
             ActivityType newActivityType = i.getArgument(0);
             if (!activityTypeMockTable.contains(newActivityType)) {
                 ReflectionTestUtils.setField(newActivityType, "id", DEFAULT_ACTIVITY_TYPE_ID + activityTypeCount++);
@@ -69,6 +57,17 @@ class ActivityTypeControllerTest {
         when(activityTypeRepository.findAll()).thenAnswer(i -> {
             return activityTypeMockTable;
         });
+        // FindActivityTypeByName
+        when(activityTypeRepository.findActivityTypeByName(Mockito.any(String.class))).thenAnswer(i -> {
+            ActivityType foundActtivityType = null;
+            for (ActivityType activityType: activityTypeMockTable) {
+                if (activityType.getName().equals(i.getArgument(0))) {
+                    foundActtivityType = activityType;
+                    break;
+                }
+            }
+            return foundActtivityType;
+        });
     }
 
     @BeforeEach
@@ -84,30 +83,38 @@ class ActivityTypeControllerTest {
         activityTypeTestList.add(new ActivityType("Biking"));
 
         activityTypeRepository.saveAll(activityTypeTestList);
+        activityTypeService = new ActivityTypeService(activityTypeRepository);
     }
 
 
     // ----------- Tests -----------
 
-    /**
-     * Checks the ActivityTypes from GET "/activity-types" endpoint are correct.
-     */
     @Test
-    void retrieveActivityTypesThroughEndpoint() throws Exception {
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/activity-types")
-                .header("Token", validToken);
+    void replacesEntitiesFromRepository() {
+        Set<ActivityType> detachedActivityTypes = new HashSet<>();
+        detachedActivityTypes.add(new ActivityType("Skiing"));
+        detachedActivityTypes.add(new ActivityType("Trail Running"));
 
-        MvcResult result = mvc.perform(request)
-                .andExpect(status().isOk())
-                .andReturn();
+        System.out.println(activityTypeRepository.findAll());
 
-        // Deserialize json response into set of ActivityTypes
-        Set<ActivityType> receivedActivityTypeList = new HashSet<>();
-        String jsonResponseStr = result.getResponse().getContentAsString();
-        for (JsonNode activityTypeNode: objectMapper.readTree(jsonResponseStr)) {
-            receivedActivityTypeList.add( objectMapper.treeToValue(activityTypeNode, ActivityType.class) );
+        Set<ActivityType> attachedActivityTypes = activityTypeService.getMatchingEntitiesFromRepository(
+                detachedActivityTypes);
+
+        // Get from Set
+        ActivityType skiing = null, trailRunning = null;
+        for (ActivityType activityType: attachedActivityTypes) {
+            if (activityType.getName().equals("Skiing")) {
+                skiing = activityType;
+            }
+            if (activityType.getName().equals("Trail Running")) {
+                trailRunning = activityType;
+            }
         }
-
-        assertEquals(new HashSet<>(activityTypeRepository.findAll()), receivedActivityTypeList);
+        assertNotNull(skiing);
+        assertNotNull(trailRunning);
+        assertEquals(1L, skiing.getId());
+        assertEquals(2L, trailRunning.getId());
     }
+
+
 }
