ccf77b3d4dea466873c0898f0998871d94141925
Taran Jennison <tmj49@uclive.ac.nz>
2020-07-13 20:22:00 +1200

    Staring work on UserValidator class
    Pulled basic validation logic out of user class and into UserValidation class. Still needs to be seperated into functions and is not yet functional #story[318]

--- server/src/main/java/com/springvuegradle/seng302team600/model/User.java
+++ server/src/main/java/com/springvuegradle/seng302team600/model/User.java
@@ -26,9 +26,6 @@ public class User {
 
     final static public int MAX_EMAILS = 5;
 
-    final static public int MIN_AGE = 13;
-    final static public int MAX_AGE = 150;
-
     final static private int FIELD_LEN = 15;
 
     @Id
@@ -437,52 +434,6 @@ public class User {
     public int getRole() {
         return role;
     }
-
-    /**
-     * Runs a sanity check on the user and throws errors if the are invalid fields
-     * @throws ResponseStatusException thrown if the users first, middle or last names are invalid
-     * thrown if the user is younger than 13
-     * thrown if the user is older than 150yr
-     * @return returns true if valid user
-     */
-    public boolean isValid() {
-        final String nameRegex = "^[a-zA-Z_]+([a-zA-Z_]+)*$";
-        final String middleNameRegex = "^[a-zA-Z_]*$";
-        String nameError = "Name must contain at least one letter and no non-letter characters";
-        if (firstName == null || lastName == null) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid name. " + nameError); }
-        if (! firstName.matches(nameRegex) ) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid first name. " + nameError); }
-        if (! lastName.matches(nameRegex) ) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid last name. " + nameError); }
-        if (middleName != null) {
-            if (! middleName.matches(middleNameRegex) && ! middleName.trim().isEmpty() ) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid middle name. " + nameError); }
-        }
-        if (ageCheck(dateOfBirth, MIN_AGE, true)) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You must be at least 13 years old to register for this app"); }
-        if (ageCheck(dateOfBirth, MAX_AGE, false)) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date of birth"); }
-        return true;
-    }
-
-    /**
-     * Checks if the given date of birth would result in the user being younger or older than the given age
-     * @param DoB date of birth for prespective new user
-     * @param age age to check against
-     * @param younger boolean tag to determine if checking if the person is younger or older (false:older, true:younger)
-     * @return boolean tag denoting how given DoB compares to given age with respect to younger tag
-     */
-    private boolean ageCheck(Date DoB, int age, boolean younger) {
-        Calendar calendar = Calendar.getInstance();
-        //Lock calender time to end of day to ensure comparison is accurate and reliable
-        calendar.set(Calendar.HOUR_OF_DAY, 23);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 59);
-        calendar.set(Calendar.MILLISECOND, 99);
-        calendar.add(Calendar.YEAR, -age);
-        Date ageDate = calendar.getTime();
-        if ( younger ) {
-            return ageDate.before(DoB);
-        } else {
-            return ageDate.after(DoB);
-        }
-    }
-
 
     @Override
     public String toString() {
