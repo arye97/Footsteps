1af22fb8cb0f459a35514a2cdc2edfdf88cc9aba
Ryan Franks <ref48@uclive.ac.nz>
2020-07-13 15:20:11 +1200

    GET Endpoint for getting activities from a user from user id #story[326]
    GET Endpoint for getting activities from a user from user id, wrote manual SQL to get the list of activities, and wrote tests in ActivityControllerTests and all still pass

--- server/src/main/java/com/springvuegradle/seng302team600/repository/ActivityRepository.java
+++ server/src/main/java/com/springvuegradle/seng302team600/repository/ActivityRepository.java
@@ -3,11 +3,16 @@ package com.springvuegradle.seng302team600.repository;
 import com.springvuegradle.seng302team600.model.Activity;
 import com.springvuegradle.seng302team600.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-
     Activity findByActivityId(Long id);
 
+    //If you see that this is highlighted red, it is NOT AN ERROR, please don't delete.
+    @Query(value = "SELECT * FROM activity WHERE creator_user_id = ?1 ORDER BY activity_id ASC", nativeQuery = true)
+    List<Activity> findAllByUserId(Long userId);
 }
