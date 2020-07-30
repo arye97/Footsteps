af8a6cf4d171e9a1617d4fe9935a2d40d70abbcb
Taran Jennison <tmj49@uclive.ac.nz>
2020-07-31 10:31:07 +1200

    Created non-functional search page
    Currently just has selection boxes and has a card below that displays current search options and selections

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -13,24 +13,71 @@
     </header>
 
     <b-container class="contents">
+        <Header/>
+        <br/>
         <div class="container h-100">
-            <b-form-input v-model="searchTerm" placeholder="Enter your name"></b-form-input>
-            <b-button style="margin-bottom: 1.7em; margin-top: 0.8em" variant="primary" v-on:click="search(searchTerm)">Search</b-button>
+            <b-row>
+                <b-col cols="8">
+                    <multiselect v-model="selectedActivityTypes" id="activityTypes" v-on:select="unDisplayRules"
+                                 :options="activityTypes" :multiple="true" :searchable="true" :close-on-select="false"
+                                 placeholder="Select your activity types">
+                        <template slot="noResult">Invalid activity type</template>
+                    </multiselect>
+                </b-col>
+                <b-col cols="4">
+                    <b-form-select v-model="searchMode" :options="searchModes"></b-form-select>
+                </b-col>
+            </b-row>
+            <b-row style="margin-bottom: 1.7em; margin-top: 0.8em">
+                <b-col cols="2" align-self="center">
+                    <b-button variant="primary" v-on:click="search()">
+                        Search</b-button>
+                </b-col>
+                <b-col cols="4" align-self="center">
+                    <b-form-radio v-model="searchType" name="andType" value="and">Search including all</b-form-radio>
+                </b-col>
+                <b-col cols="4" align-self="center">
+                    <b-form-radio v-model="searchType" name="orType" value="or">Search including some</b-form-radio>
+                </b-col>
+            </b-row>
         </div>
         <div style="text-align: center">
-            <b-button style="margin-bottom: 1.7em; margin-top: 0.8em" variant="primary" v-on:click="goToPage('/profile')">Profile</b-button>
+            <b-card>
+                <b-card-text>{{ searchMode }}</b-card-text>
+                <b-card-text>{{ selectedActivityTypes }}</b-card-text>
+                <b-card-text>{{ searchType }}</b-card-text>
+            </b-card>
+
         </div>
     </b-container>
     </div>
 </template>
 
 <script>
+    import Header from '../components/Header/Header.vue';
+    import Multiselect from 'vue-multiselect'
+
     export default {
         name: "Search",
+        components: {
+            Header,
+            Multiselect
+        },
 
         data() {
             return {
-                searchTerm : ""
+                searchMode: 'activityType',
+                searchModes: [
+                    { value: 'activityType', text: 'Activity Type'}
+                ],
+                selectedActivityTypes : [],
+                activityTypes: [
+                    "Archery",
+                    "E-sports",
+                    "Fencing",
+                    "Boating"
+                ],
+                searchType: "and"
             }
         },
 
@@ -39,8 +86,8 @@
                 this.$router.push(url);
             },
 
-            search(searchTerm) {
-                console.log(searchTerm);
+            search() {
+                console.log(this.selectedActivityTypes);
             }
         }
     }
