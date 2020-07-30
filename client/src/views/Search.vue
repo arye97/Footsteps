6c951b3ace15493e5a266be6811acc0a5a77c16d
Taran Jennison <tmj49@uclive.ac.nz>
2020-07-30 16:41:35 +1200

    Created simple search box and button
    Have realised that I need help on

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,15 +1,51 @@
 <template>
     <div>
-        <router-link
-                :to="{ name: 'myProfile' }"
-                class="nav-link">Profile</router-link>
+    <header class="masthead">
+        <br/><br/><br/>
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12 text-center">
+                    <Header/>
+                    <br/>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <b-container class="contents">
+        <div class="container h-100">
+            <b-form-input v-model="searchTerm" placeholder="Enter your name"></b-form-input>
+            <b-button style="margin-bottom: 1.7em; margin-top: 0.8em" variant="primary" v-on:click="search(searchTerm)">Search</b-button>
+        </div>
+        <div style="text-align: center">
+            <b-button style="margin-bottom: 1.7em; margin-top: 0.8em" variant="primary" v-on:click="goToPage('/profile')">Profile</b-button>
+        </div>
+    </b-container>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Search"
+        name: "Search",
+
+        data() {
+            return {
+                searchTerm : ""
+            }
+        },
+
+        methods: {
+            goToPage(url) {
+                this.$router.push(url);
+            },
+
+            search(searchTerm) {
+                console.log(searchTerm);
+            }
+        }
     }
+
+
 </script>
 
 <style scoped>
