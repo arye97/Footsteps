222d9a5a451b8e054113b0be99a01da012f9f5bd
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-08-01 12:20:42 +1200

    EditEmail.test.js Checks if the page displays the primary email.  #story[369]

--- client/src/Test/EditEmail.test.js
+++ client/src/Test/EditEmail.test.js
@@ -1,35 +1,55 @@
 import {shallowMount} from '@vue/test-utils'
 import EditEmail from '../views/Settings/EditEmail.vue'
+import api from "../Api"
 import 'vue-jest'
+jest.mock("../Api");
 
 let editEmail;
 let push;
-let config;
+
 const DEFAULT_USER_ID = 1;
 
-beforeAll(() => {
+
+beforeEach(async () => {
     push = jest.fn();
     const $router = {
         push: jest.fn()
     };
     const $route = {
-        params: {userId : DEFAULT_USER_ID},
+        params: {userId : undefined},
     };
+
+    api.getUserData.mockImplementation(
+        () => Promise.resolve({ data: {id: DEFAULT_USER_ID}, status: 201 }))
+
+    api.getUserEmails.mockImplementation(
+        () => Promise.resolve({
+            data: {
+                userId: DEFAULT_USER_ID,
+                additionalEmails: ["johnhopkins@hotmail.com", "stewart@yahoo.com"],
+                primaryEmail: "johntester@tester.com"
+            },
+            status: 200
+        }));
 
     editEmail = shallowMount(EditEmail, {
         methods: {
-            editable: async function () {}
+            editable: async () => {},
+            logout: () => {}
         },
         mocks: {
             $route,
             $router
         }
     });
-    editEmail.vm.$data.isEditable = true;
-
 });
 
 test('Is a vue instance', () => {
     expect(editEmail.exists()).toBeTruthy();
     expect(editEmail.isVueInstance).toBeTruthy();
+});
+
+
+test('Displays Primary Email', () => {
+    expect(editEmail.find("#primaryEmail").text()).toBe("johntester@tester.com");
 });
