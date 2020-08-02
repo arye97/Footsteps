d56a86ca3580307e24019143313c5bbe2405dd94
Rebekah McKinnon <rmc209@uclive.ac.nz>
2020-08-03 10:12:02 +1200

    Updated to now use a one table styled approach. Some commented lines as waiting on review of feed post enum before this can be properly included #story[382]

--- server/src/main/java/com/springvuegradle/seng302team600/model/FeedEvent.java
+++ server/src/main/java/com/springvuegradle/seng302team600/model/FeedEvent.java
@@ -1,23 +1,29 @@
 package com.springvuegradle.seng302team600.model;
 
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
  * The superclass for feed events
  * All feed events will 'extend' this class
  */
-@MappedSuperclass
+@Entity
+@Table(name = "feed_event")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name="postType", discriminatorType = FeedPostType)
 public class FeedEvent {
 
     // The ID of the event, will be auto-set by Spring
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feed_event_id", nullable = false)
     Long eventId;
 
     // The type of feed post, from the enumeration
+    // May not need this as the discriminator column does this for us
     // FeedPostType postType;
 
     // The timestamp of the feed event generation - ie. when the feed post was triggered
+    @Column(name = "time_stamp", nullable = false, columnDefinition = "DATE")
     Date timeStamp;
 }
