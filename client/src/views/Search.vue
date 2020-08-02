de60fd27ed6dc4543be2bc5e06f283015647c6e7
Ethan Burnett <ebb35@uclive.ac.nz>
2020-08-02 15:53:41 +1200

    Created user card for displaying basic user info. - First and last name - Primary email - Bio (first 75 chars) - Activity types included in the query #story[381]

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -41,13 +41,44 @@
                 </b-col>
             </b-row>
         </div>
-        <div style="text-align: center">
-            <b-card>
-                <b-card-text>{{ searchMode }}</b-card-text>
-                <b-card-text>{{ selectedActivityTypes }}</b-card-text>
-                <b-card-text>{{ searchType }}</b-card-text>
+        <section v-for="user in this.userList" :key="user.id">
+            <!-- User List -->
+            <b-card border-variant="secondary" style="background-color: #f3f3f3">
+                <b-row class="mb-1">
+                    <b-col>
+                        <b-card-text><strong>{{ user.firstname }} {{ user.lastname }}</strong></b-card-text>
+                    </b-col>
+                    <b-col>
+                        <!-- View user button -->
+                        <b-button id="viewProfileButton" style="float: right" variant="primary" v-on:click="viewProfile(user.id)">View Profile</b-button>
+                    </b-col>
+                </b-row>
+                <hr style="border-color: inherit">
+                <b-row class="mb-1">
+                    <b-col>
+                        <strong>Email: </strong>{{ user.emails[0].email }}
+                        <br/><br/>
+                        <div v-if="user.bio.length <= 75">
+                            {{ user.bio }}
+                        </div>
+                        <div v-else>
+                            {{ user.bio.substring(0,75)+"...." }}
+                        </div>
+                    </b-col>
+                    <b-col v-if="user.activityTypes.length >= 1">
+                        <b-list-group>
+                            <section v-for="activityType in user.activityTypes" v-bind:key="activityType">
+                                <!-- Only display queried activity types -->
+                                <b-list-group-item v-if="selectedActivityTypes.indexOf(activityType.name) > -1" variant="success">
+                                    {{ activityType.name }}
+                                </b-list-group-item>
+                            </section>
+                        </b-list-group>
+                    </b-col>
+                </b-row>
             </b-card>
-        </div>
+            <br>
+        </section>
     </b-container>
     </div>
 </template>
@@ -66,6 +97,7 @@
         //todo: replace b-card on lines 45-49 with user cards. Current card is just for debugging/testing
         data() {
             return {
+                userList: [],
                 searchMode: 'activityType',
                 searchModes: [  //can be expanded to allow for different searching mode (ie; search by username, email... etc)
                     { value: 'activityType', text: 'Activity Type'}
@@ -90,6 +122,13 @@
             },
 
             /**
+             * Function for redirecting to user profile via userId
+             */
+            viewProfile(userId) {
+                this.goToPage({ name: 'profile', params: {userId: userId} })
+            },
+
+            /**
              * Searches a user based on a string of activity types and a method AND or OR
              * @param searchTerm a string of activity types
              * @param method a string indicating AND or OR
@@ -103,6 +142,7 @@
                         if (response.status === 200) {
                             console.log(response.data)
                             // Show users in page
+                            this.userList = response.data;
                         }
                     }).catch(err => {
                         if (err.response.status === 401) {
