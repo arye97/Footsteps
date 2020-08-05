9db75d7068c30da05b01ae4c9da38b4d5bbecee9
Rebekah McKinnon <rmc209@uclive.ac.nz>
2020-08-05 12:17:06 +1200

    Merge the enum in from dev and update accordingly for the discriminator column #story[382]

--- server/src/main/java/com/springvuegradle/seng302team600/model/FeedEvent.java
+++ server/src/main/java/com/springvuegradle/seng302team600/model/FeedEvent.java
@@ -6,11 +6,12 @@ import java.util.Date;
 /**
  * The superclass for feed events
  * All feed events will 'extend' this class
+ * The discriminator column is filled in by the subclasses and will use the enumeration FeedPostType
  */
 @Entity
 @Table(name = "feed_event")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-//@DiscriminatorColumn(name="postType", discriminatorType = FeedPostType)
+@DiscriminatorColumn(name="postType")
 public class FeedEvent {
 
     // The ID of the event, will be auto-set by Spring
@@ -18,10 +19,6 @@ public class FeedEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feed_event_id", nullable = false)
     Long eventId;
-
-    // The type of feed post, from the enumeration
-    // May not need this as the discriminator column does this for us
-    // FeedPostType postType;
 
     // The timestamp of the feed event generation - ie. when the feed post was triggered
     @Column(name = "time_stamp", nullable = false, columnDefinition = "DATE")
