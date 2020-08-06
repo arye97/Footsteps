6f4b6b19679be327673242ea7468b96c2b67a949
Ethan Burnett <ebb35@uclive.ac.nz>
2020-08-07 10:07:56 +1200

    Began implementation of FeedEventRepository, waiting on FeedEvent to be completed. Also, began implementation of FeedEventController, waiting on Repository. Only the stub methods have been placed within controller. Also, optimised imports in ActivityController. #story[382]

---
+++ server/src/main/java/com/springvuegradle/seng302team600/controller/FeedEventController.java
@@ -0,0 +1,43 @@
+package com.springvuegradle.seng302team600.controller;
+
+import com.springvuegradle.seng302team600.payload.IsFollowingResponse;
+import com.springvuegradle.seng302team600.repository.FeedEventRepository;
+import com.springvuegradle.seng302team600.service.UserAuthenticationService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * FeedEventController Contains the endpoints for GET, POST and DELETE request for the client.
+ * The FeedEventRepository is the access point to the database for querying Feed Events.
+ */
+@RestController
+public class FeedEventController {
+
+    private final UserAuthenticationService userService;
+    private final FeedEventRepository feedEventRepository;
+
+    public FeedEventController(UserAuthenticationService userService, FeedEventRepository feedEventRepository) {
+        this.userService = userService;
+        this.feedEventRepository = feedEventRepository;
+    }
+
+    @PostMapping("/profiles/{profileId}/subscriptions/activities/{activityId}")
+    public void followAnActivity(HttpServletRequest request, HttpServletResponse response,
+                                 @PathVariable Long profileId, @PathVariable Long activityId) {
+        //ToDo This method is already implemented, ask Ryan F, also add DocString
+    }
+
+    @DeleteMapping("/profiles/{profileId/subscriptions/activities/{activityId}")
+    public void unFollowAnActivity(HttpServletRequest request, HttpServletResponse response,
+                                   @PathVariable Long profileId, @PathVariable Long activityId) {
+        //ToDo Implement this method, also add DocString
+    }
+
+    @GetMapping()
+    public IsFollowingResponse isFollowingAnActivity() {
+        //ToDo Implement this method, also add DocString
+        return null;
+    }
+}
