f82b27adcc0c40fadde7f4751cb3febad3994a86
Rebekah McKinnon <rmc209@uclive.ac.nz>
2020-08-09 15:24:08 +1200

    Merge branch 'development' of C:\Users\rebek\OneDrive - University of Canterbury\SENG302\Project with conflicts.

---
+++ client/src/components/HomeFeed/HomeFeed.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="app">
+        <section v-if="errored" class="text-center">
+            <div class="alert alert-danger alert-dismissible fade show text-center" role="alert" id="alert">
+                {{ error_message }}
+            </div>
+        </section>
+        <section v-else-if="loading" class="text-center">
+            <br/>
+            <b-spinner variant="primary" label="Spinning"></b-spinner>
+            <br/>
+            <br/>
+        </section>
+        <section v-else v-for="event in this.currentPageEventList" :key="event.id">
+            <!-- User List -->
+            <feed-card v-bind:event="event"/>
+            <br>
+        </section>
+        <!-- Pagination Nav Bar -->
+        <b-pagination
+                v-if="!errored && !loading && feedEventList.length >= 1"
+                align="fill"
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="eventsPerPage"
+        ></b-pagination>
+    </div>
+</template>
+
+<script>
+    import FeedCard from "./FeedCard";
+    export default {
+        name: "HomeFeed",
+        components: {
+            FeedCard
+        },
+        data: function() {
+            return {
+                errored: false,
+                loading: false,
+                feedEventList: [],
+                currentPage: 1,
+                eventsPerPage:10,
+                currentPageEventList: []
+            }
+        },
+        computed: {
+            rows() {
+                return this.feedEventList.length;
+            }
+        },
+        mounted() {
+            //TODO - Replace this with an api call when endpoints are generated
+            this.feedEventList = [
+                {
+                    id: 1,
+                    feedEventType: 'FOLLOW',
+                    timeStamp: new Date("August 07, 2020 17:00:00"),
+                    activityId: 1,
+                    userId: 1
+                },
+                {
+                    id: 2,
+                    feedEventType: 'MODIFY',
+                    timeStamp: new Date("August 09, 2020 14:56:30"),
+                    activityId: 50,
+                    userId: 4
+                },
+                {
+                    id: 3,
+                    feedEventType: 'DELETE',
+                    timeStamp: new Date(),
+                    activityId: 50,
+                    userId: 4
+                }
+            ];
+            // Flip the list order so the most recent event shows first
+            this.feedEventList.reverse();
+            this.currentPageEventList = this.feedEventList;
+        }
+    }
+</script>
