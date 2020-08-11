907f0710b90ffbf1cadac0a2956e7024f90cf7f5
Ryan Franks <ref48@uclive.ac.nz>
2020-08-11 16:33:02 +1200

    Fixed findAllByOutcomeId function to be more spring optimized #story[384]

--- server/src/main/java/com/springvuegradle/seng302team600/repository/ResultRepository.java
+++ server/src/main/java/com/springvuegradle/seng302team600/repository/ResultRepository.java
@@ -1,12 +1,7 @@
 package com.springvuegradle.seng302team600.repository;
 
-
-import com.springvuegradle.seng302team600.model.Activity;
 import com.springvuegradle.seng302team600.model.Result;
-import com.springvuegradle.seng302team600.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -14,6 +9,5 @@ import java.util.List;
 @RepositoryRestResource
 public interface ResultRepository extends JpaRepository<Result, Long> {
 
-    @Query(value = "SELECT * FROM result WHERE outcome_id = ?1", nativeQuery=true)
-    List<Result> findAllResultsByOutcomeId(@Param("outcome_id") Long outcome_id);
+    List<Result> findAllByOutcomeId(Long outcomeId);
 }
