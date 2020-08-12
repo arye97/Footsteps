afa09a898cc18a86dfec21b6db0d2406b05f1113
Ethan Burnett <ebb35@uclive.ac.nz>
2020-08-12 12:06:04 +1200

    Created ResultController and the stub methods to be implemented. Also implemented mocking for ResultControllerTest, however more mocking could be done when creating tests. #story[384]

--- server/src/main/java/com/springvuegradle/seng302team600/controller/ResultController.java
+++ server/src/main/java/com/springvuegradle/seng302team600/controller/ResultController.java
@@ -1,24 +1,49 @@
 package com.springvuegradle.seng302team600.controller;
 
+import com.springvuegradle.seng302team600.model.Result;
+import com.springvuegradle.seng302team600.repository.ActivityParticipantRepository;
 import com.springvuegradle.seng302team600.repository.ActivityRepository;
 import com.springvuegradle.seng302team600.repository.OutcomeRepository;
 import com.springvuegradle.seng302team600.repository.ResultRepository;
 import com.springvuegradle.seng302team600.service.UserAuthenticationService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 public class ResultController {
 
     private final UserAuthenticationService userAuthenticationService;
     private final OutcomeRepository outcomeRepository;
-    private final ActivityRepository activityRepository;
     private final ResultRepository resultRepository;
+    private final ActivityParticipantRepository participantRepository;
+    private final ActivityRepository activityRepository;
 
 
-    public ResultController(UserAuthenticationService userAuthenticationService, OutcomeRepository outcomeRepository, ActivityRepository activityRepository, ResultRepository resultRepository) {
+    public ResultController(UserAuthenticationService userAuthenticationService, OutcomeRepository outcomeRepository,
+                            ResultRepository resultRepository, ActivityParticipantRepository participantRepository, ActivityRepository activityRepository) {
         this.userAuthenticationService = userAuthenticationService;
         this.outcomeRepository = outcomeRepository;
-        this.activityRepository = activityRepository;
         this.resultRepository = resultRepository;
+        this.participantRepository = participantRepository;
+        this.activityRepository = activityRepository;
+    }
+
+    @PostMapping("/outcomes/{outcomeId}/results")
+    public void createNewOutcomeResult(@Validated @RequestBody Result result, @PathVariable Long outcomeId,
+                                       HttpServletRequest request, HttpServletResponse response) {
+        //TODO Implement this method, and add a DocString please
+        // Must check if user is a participant
+        // Must set the Outcome for this Result
+    }
+
+    @GetMapping("/outcomes/{outcomeId}/results")
+    public List<Result> getOutcomeResults(@PathVariable Long outcomeId, HttpServletRequest request,
+                                          HttpServletResponse response) {
+        //TODO Implement this method, and add a DocString please
+        return null;
     }
 }
