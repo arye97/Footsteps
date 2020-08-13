c0942a500829d58e1ab1ce417e094ad06337147d
Rebekah McKinnon <rmc209@uclive.ac.nz>
2020-08-14 00:29:13 +1200

    Add jest tests to the activity page #story[384]

--- client/src/Test/ViewActivity.test.js
+++ client/src/Test/ViewActivity.test.js
@@ -7,86 +7,270 @@ jest.mock('../Api');
 
 let viewActivity;
 
-const CONTINUOUS_ACTIVITY_DATA = {
-    activity_name: "My activity",
-    description: "An activity description",
-    creatorUserId: 1,
-    location: "Activity location",
-    start_time: new Date(),
-    end_time: new Date(),
-    activity_type: [
-        {"activityTypeId":1, "name":"Free Running"},
-        {"activityTypeId":2, "name":"Walking"}
-    ],
-    continuous: true
-};
-const ACTIVE_USER_DATA = 1;
-const SUBSCRIBED_DATA = {"subscribed":false};
-const CREATOR_USER_DATA = {
-    "lastname":"Tomato",
-    "firstname":"Bob",
-    "middlename":"The",
-    "nickname":"Bobby",
-    "primary_email":"bob@thetomato.com",
-    "additional_email":["bob@bob.com"],
-    "activityTypes":[],
-    "id":18,
-    "passports":["Algeria","Bolivia (Plurinational State of)","Holy See","Honduras"],
-    "fitness":-1,
-    "gender":"Male",
-    "date_of_birth":"1987-04-04",
-    "bio":"My name is Bob"
+let CONTINUOUS_ACTIVITY_DATA;
+let ACTIVE_USER_DATA;
+let SUBSCRIBED_DATA;
+let CREATOR_USER_DATA;
+
+const $route = {
+    params: {
+        activityId: 1
+    }
 };
 
-beforeEach(() => {
-    api.getActivityData.mockImplementation(() => {
-        return Promise.resolve({
-            data: CONTINUOUS_ACTIVITY_DATA,
-            status: 200
-        });
-    });
-    api.getUserId.mockImplementation(() => {
-        return Promise.resolve({
-            data: ACTIVE_USER_DATA,
-            status: 200
-        });
-    });
-    api.getUserSubscribed.mockImplementation(() => {
-        return Promise.resolve({
-            data: SUBSCRIBED_DATA,
-            status: 200
-        });
-    });
-    api.getUserData.mockImplementation(() => {
-        return Promise.resolve({
-            data: CREATOR_USER_DATA,
-            status: 200
-        });
-    });
-    viewActivity = mount(ViewActivity, {
-        mocks: {api}
-    });
-});
+/**
+ * A function to cause a delay before a promise is resolved
+ * @param milliseconds time to delay
+ * @returns {Promise<any>} code to execute after delay
+ */
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
+
+/**
+ * Sets the values for the api response mocks
+ */
+const setValues = () => {
+    CONTINUOUS_ACTIVITY_DATA = {
+        activity_name: "My activity",
+        description: "An activity description",
+        creatorUserId: 18,
+        location: "Activity location",
+        start_time: new Date(),
+        end_time: new Date(),
+        activity_type: [
+            {"activityTypeId":1, "name":"Free Running"},
+            {"activityTypeId":2, "name":"Walking"}
+        ],
+        continuous: true
+    };
+    ACTIVE_USER_DATA = 18;
+    SUBSCRIBED_DATA = {"subscribed":false};
+    CREATOR_USER_DATA = {
+        "lastname":"Tomato",
+        "firstname":"Bob",
+        "middlename":"The",
+        "nickname":"Bobby",
+        "primary_email":"bob@thetomato.com",
+        "additional_email":["bob@bob.com"],
+        "activityTypes":[],
+        "passports":["Algeria","Bolivia (Plurinational State of)","Holy See","Honduras"],
+        "fitness":-1,
+        "gender":"Male",
+        "date_of_birth":"1987-04-04",
+        "bio":"My name is Bob"
+    };
+};
 
 test('Is a vue instance', () => {
+    viewActivity = mount(ViewActivity);
     expect(viewActivity.isVueInstance).toBeTruthy();
 });
 
 describe('The view activity page', () => {
+    beforeEach(() => {
+        setValues();
+        return new Promise(resolve => {
+            api.getActivityData.mockImplementation(() => {
+                return Promise.resolve({
+                    data: CONTINUOUS_ACTIVITY_DATA,
+                    status: 200
+                });
+            });
+            api.getUserId.mockImplementation(() => {
+                return Promise.resolve({
+                    data: ACTIVE_USER_DATA,
+                    status: 200
+                });
+            });
+            api.getUserSubscribed.mockImplementation(() => {
+                return Promise.resolve({
+                    data: SUBSCRIBED_DATA,
+                    status: 200
+                });
+            });
+            api.getUserData.mockImplementation(() => {
+                return Promise.resolve({
+                    data: CREATOR_USER_DATA,
+                    status: 200
+                });
+            });
+            viewActivity = mount(ViewActivity, {
+                mocks: {
+                    api,
+                    $route
+                }
+            });
+
+            // This causes a delay between beforeEach finishing, and the tests being run.
+            // This isn't at all good practice, but its the only way I am able
+            // to get ViewActivity to fully mount before the tests are run.
+            // resolve() signals the above promise to complete
+            sleep(300).then(() => resolve());
+        });
+    });
+
     test('displays the activity title', () => {
-        expect(viewActivity.find('#activityTitle')).toBeTruthy();
+        expect(viewActivity.find('#activityTitle').exists()).toBeTruthy();
     });
 
     test('displays the activity description', () => {
-        expect(viewActivity.find('#description')).toBeTruthy();
+        expect(viewActivity.find('#description').exists()).toBeTruthy();
     });
 
     test('displays the activity location', () => {
-        expect(viewActivity.find('#location')).toBeTruthy();
+        expect(viewActivity.find('#location').exists()).toBeTruthy();
     });
 
-    // test('does not display the start time for continuous events', () => {
-    //     console.log(viewActivity.html());
-    //     expect(viewActivity.find('#startTime')).toBeFalsy();
-    // });
+    test('has a view participants button', () => {
+        expect(viewActivity.find('#viewParticipants').exists()).toBeTruthy();
+    });
+
+    test('has a view results button', () => {
+        expect(viewActivity.find('#viewResults').exists()).toBeTruthy();
+    });
+
+    test('shows the activity type', () => {
+        expect(viewActivity.find('#activityType').exists()).toBeTruthy();
+    });
+});
+
+describe('As an activity creator', () => {
+    test('I cannot see the follow button', () => {
+        expect(viewActivity.find('#followButton').exists()).toBeFalsy();
+    });
+
+    test('I cannot see the unfollow button', () => {
+        expect(viewActivity.find('#unfollowButton').exists()).toBeFalsy();
+    });
+
+    test('I can see the edit activity button', () => {
+        expect(viewActivity.find('#editActivity').exists()).toBeTruthy();
+    });
+
+    test('I can see the add results button', () => {
+        expect(viewActivity.find('#addResults').exists()).toBeTruthy();
+    });
+});
+
+describe("If I'm following an activity", () => {
+    beforeEach(() => {
+        ACTIVE_USER_DATA = 2;
+        SUBSCRIBED_DATA = {"subscribed":true};
+        return new Promise(resolve => {
+            api.getActivityData.mockImplementation(() => {
+                return Promise.resolve({
+                    data: CONTINUOUS_ACTIVITY_DATA,
+                    status: 200
+                });
+            });
+            api.getUserId.mockImplementation(() => {
+                return Promise.resolve({
+                    data: ACTIVE_USER_DATA,
+                    status: 200
+                });
+            });
+            api.getUserSubscribed.mockImplementation(() => {
+                return Promise.resolve({
+                    data: SUBSCRIBED_DATA,
+                    status: 200
+                });
+            });
+            api.getUserData.mockImplementation(() => {
+                return Promise.resolve({
+                    data: CREATOR_USER_DATA,
+                    status: 200
+                });
+            });
+            viewActivity = mount(ViewActivity, {
+                mocks: {
+                    api,
+                    $route
+                }
+            });
+
+            // This causes a delay between beforeEach finishing, and the tests being run.
+            // This isn't at all good practice, but its the only way I am able
+            // to get ViewActivity to fully mount before the tests are run.
+            // resolve() signals the above promise to complete
+            sleep(300).then(() => resolve());
+        });
+    });
+
+    test('I can see the unfollow button', () => {
+        expect(viewActivity.find('#unfollowButton').exists()).toBeTruthy();
+    });
+
+    test("I can't see the follow button", () => {
+        expect(viewActivity.find('#followButton').exists()).toBeFalsy();
+    });
+
+    test("I can't see the edit activity button", () => {
+        expect(viewActivity.find('#editActivity').exists()).toBeFalsy();
+    });
+
+    test('I can see the add results button', () => {
+        expect(viewActivity.find('#addResults').exists()).toBeTruthy();
+    });
+});
+
+describe('If I am not following the activity', () => {
+    beforeEach(() => {
+        ACTIVE_USER_DATA = 2;
+        SUBSCRIBED_DATA = {"subscribed":false};
+        return new Promise(resolve => {
+            api.getActivityData.mockImplementation(() => {
+                return Promise.resolve({
+                    data: CONTINUOUS_ACTIVITY_DATA,
+                    status: 200
+                });
+            });
+            api.getUserId.mockImplementation(() => {
+                return Promise.resolve({
+                    data: ACTIVE_USER_DATA,
+                    status: 200
+                });
+            });
+            api.getUserSubscribed.mockImplementation(() => {
+                return Promise.resolve({
+                    data: SUBSCRIBED_DATA,
+                    status: 200
+                });
+            });
+            api.getUserData.mockImplementation(() => {
+                return Promise.resolve({
+                    data: CREATOR_USER_DATA,
+                    status: 200
+                });
+            });
+            viewActivity = mount(ViewActivity, {
+                mocks: {
+                    api,
+                    $route
+                }
+            });
+
+            // This causes a delay between beforeEach finishing, and the tests being run.
+            // This isn't at all good practice, but its the only way I am able
+            // to get ViewActivity to fully mount before the tests are run.
+            // resolve() signals the above promise to complete
+            sleep(300).then(() => resolve());
+        });
+    });
+
+    test("I can't see the unfollow button", () => {
+        expect(viewActivity.find('#unfollowButton').exists()).toBeFalsy();
+    });
+
+    test("I can see the follow button", () => {
+        expect(viewActivity.find('#followButton').exists()).toBeTruthy();
+    });
+
+    test("I can't see the edit activity button", () => {
+        expect(viewActivity.find('#editActivity').exists()).toBeFalsy();
+    });
+
+    test("I can't see the add results button", () => {
+        expect(viewActivity.find('#addResults').exists()).toBeFalsy();
+    });
 });
