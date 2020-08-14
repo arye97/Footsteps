06e268740f8e727dcfb988bfd41c7cf716ffbde3
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-08-15 11:56:44 +1200

    Wrote test for CreateActivity.vue and fixed test in LiveBackEndApi.test.js.  #story[384]

--- client/src/Test/LiveBackEnd/LiveBackEndApi.test.js
+++ client/src/Test/LiveBackEnd/LiveBackEndApi.test.js
@@ -15,7 +15,6 @@ import api from "../../Api";
 
 // User Id of the user that is being tested.
 let USER1_ID = null;
-let USER2_ID = null;
 
 let USER2_ID = null;
 
@@ -83,14 +82,10 @@ const ACTIVITY1 = {
 let OUTCOME1_ID = null;
 let UNIT1_ID = null;
 
-let OUTCOME1 = {
-    title: "Some Title",
-    description: "Some Description",
-    units: [
-        {
-            name: "Distance",
-            unit_type: "TEXT"
-        }]
+const OUTCOME1 = {
+    title: "My Awesome Outcome",
+    unit_name: "Distance",
+    unit_type: "TEXT"
 };
 
 /**
