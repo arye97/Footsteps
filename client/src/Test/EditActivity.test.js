ee9d53ba31dc6c4561cc7936c871cecbb434537a
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-08-17 12:20:41 +1200

    Fixed jest tests that were failing due to new handling of server errors in EditActivity.  #story[384]

--- client/src/Test/EditActivity.test.js
+++ client/src/Test/EditActivity.test.js
@@ -120,7 +120,8 @@ test('Catches an http status error of 403 when checking if activity can be edite
 
 
     return editActivity.vm.getActivityData().then(() => {
-        expect(spy).toHaveBeenCalledWith({name: 'allActivities'});
+        expect(spy).toHaveBeenLastCalledWith(
+            { name: 'allActivities', params: {alertCount: 5, alertMessage: "Can't get Activity data"} });
     });
 });
 
@@ -137,7 +138,8 @@ test('Catches an http status error that isnt 401 or 403 when activity data is co
 
 
     return editActivity.vm.getActivityData().then(() => {
-        expect(spy).toHaveBeenCalledWith({ name: 'myProfile' });
+        expect(spy).toHaveBeenLastCalledWith(
+            { name: 'allActivities', params: {alertCount: 5, alertMessage: "Can't get Activity data"} });
     });
 });
 
