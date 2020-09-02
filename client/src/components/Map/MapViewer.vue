d117dcd8ff7e26cf912647d2366b2317ecc236c6
Taran Jennison <tmj49@uclive.ac.nz>
2020-09-02 16:33:40 +1200

    Created MapViewer.vue and started setup for it #story[420]
    Created the basic structure of the vue file but am waiting on approval of a map display that we can use
    Also waiting on confirmation of whether the vue will pull locations from externally or if it will have methods for outer components to add to the location list

---
+++ client/src/components/Map/MapViewer.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="mapDiv">
+<!--        map goes here-->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MapViewer.vue",
+
+        data() {
+            return {
+                locations: []
+            }
+        },
+
+        async mounted() {
+
+        },
+
+        mehtods: {
+
+            /**
+             * Set the set of locations to be shown on the map
+             * @param locationsJson json formatted list of locations to be displayed on the map
+             */
+            setLocations(locationsList) {
+                //todo: check for validity of locationsJson
+                this.locations = locationsList;
+            },
+
+            /**
+             * Adds a list of json format locations to the existing locations list
+             * @param locationsList json formatted locations to be added to the current locations list
+             */
+            addLocations(locationsList) {
+                //todo: check for validity of locationsJson
+                this.locations.concat(locationsList)
+            },
+
+            /**
+             * Remove all locations from locations display list
+             */
+            clearLocations() {
+                this.locations = []
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
