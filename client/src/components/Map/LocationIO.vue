51406e3e980e93b93239b199baf8291f356f0d2b
Taran Jennison <tmj49@uclive.ac.nz>
2020-09-05 22:36:10 +1200

    Created LocationIO.vue #story[420]
    made very basic vue wrapper that contains the map pane and, for now, allows the adding of pins by co-orditantes. The adding by co-ordinates is just proof of concept and will be changed to using address searching in next commit

---
+++ client/src/components/Map/LocationIO.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <map-viewer
+                :center="center"
+                :pins="pins"
+        ></map-viewer>
+        <b-row>
+            <b-col>
+                <b-form-input
+                    id="latInput"
+                    v-model="latInput"
+                    type="number"
+                    placeholder="Input lat here"
+                ></b-form-input>
+            </b-col>
+            <b-col>
+                <b-form-input
+                        id="lngInput"
+                        v-model="lngInput"
+                        type="number"
+                        placeholder="Input lng here"
+                ></b-form-input>
+            </b-col>
+        </b-row>
+        <b-button
+            id="LocationSubmit"
+            v-on:click="addLocation()"
+        >Place pin</b-button>
+    </div>
+
+</template>
+
+<script>
+    import MapViewer from "../../components/Map/MapViewer";
+
+    export default {
+        name: "LocationIO",
+
+        components: {
+            MapViewer
+        },
+
+        data() {
+            return {
+                pins:[{id:1, lat:-43.53, lng:172.63}],
+                center:{lat:-43.53, lng:172.63},
+                latInput: 0,
+                lngInput: 0
+            }
+        },
+
+        methods: {
+            addLocation() {
+                //todo: get proper id
+                let pin = {"id":this.pins.length+1,"lat":parseFloat(this.latInput),"lng":parseFloat(this.lngInput)};
+                this.pins.push(pin);
+                this.latInput =0;
+                this.lngInput = 0;
+                console.log(process.env["VUE_APP_GOOGLE_MAPS_API_KEY"]);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
