0106b0b2515f9193334b8f8fd06aae450b073bc6
Euan Widjaja <ewi45@uclive.ac.nz>
2020-09-07 11:05:54 +1200

    Add view-only prop in LocationIO.vue to dictate whether or not map pane is editable or only readable #story[420]

--- client/src/components/Map/LocationIO.vue
+++ client/src/components/Map/LocationIO.vue
@@ -2,27 +2,27 @@
     <div>
         <b-card class="flex-fill" border-variant="secondary">
             <div v-if="isMapVisible">
-            <b-button id="hideMapButton" v-on:click="hideMap()">Hide Map</b-button>
-            <map-viewer
-                    :center="center"
-                    :pins="pins"
-            ></map-viewer>
-            <b-row>
-                <b-col>
-                    <b-form-input
-                        id="locationInput"
-                        v-model="locationInput"
-                        type="text"
-                        placeholder="Input address here"
-                    ></b-form-input>
-                </b-col>
-                <b-col>
-                    <b-button
-                            id="LocationSubmit"
-                            v-on:click="addLocation()"
-                    >Place pin</b-button>
-                </b-col>
-            </b-row>
+                <b-button id="hideMapButton" v-on:click="hideMap()">Hide Map</b-button>
+                <map-viewer
+                        :center="center"
+                        :pins="pins"
+                ></map-viewer>
+                <b-row v-if="!viewOnly">
+                    <b-col>
+                        <b-form-input
+                            id="locationInput"
+                            v-model="locationInput"
+                            type="text"
+                            placeholder="Input address here"
+                        ></b-form-input>
+                    </b-col>
+                    <b-col>
+                        <b-button
+                                id="LocationSubmit"
+                                v-on:click="addLocation()"
+                        >Place pin</b-button>
+                    </b-col>
+                </b-row>
             </div>
             <div v-else>
                 <b-button id="showMapButton" v-on:click="showMap()">Show Map</b-button>
@@ -41,13 +41,21 @@
         components: {
             MapViewer
         },
+
+        props: {
+            viewOnly: {
+                default: false,
+                type: Boolean
+            },
+        },
+
         data() {
             return {
                 isMapVisible: false,
                 pins:[{id:1, lat:-43.53, lng:172.63}],
                 center:{lat:-43.53, lng:172.63},
                 locations: [],
-                locationInput: ""
+                locationInput: "",
             }
         },
 
