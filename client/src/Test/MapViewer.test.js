a698084181113cd5e1e701d331e188a7ed37249c
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-09-09 15:26:32 +1200

    Started wrtingin jest tests for MapViewer

---
+++ client/src/Test/MapViewer.test.js
@@ -0,0 +1,48 @@
+import {mount, createLocalVue} from '@vue/test-utils'
+import MapViewer from '../components/Map/MapViewer'
+import {BootstrapVue} from 'bootstrap-vue';
+import { gmapApi } from 'gmap-vue';
+import "vue-jest"
+import * as GmapVue from "gmap-vue";
+
+jest.mock("gmap-vue");
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+// localVue.use(gmapApi);
+localVue.use(GmapVue);
+
+let config;
+
+let mapViewer;
+
+beforeEach(() => {
+    config = {
+        localVue
+    };
+    mapViewer = mount(MapViewer, config);
+});
+
+test('Is a vue instance', () => {
+    expect(mapViewer.isVueInstance).toBeTruthy();
+});
+
+test('map pane does not exist at default', () => {
+    expect(mapViewer.find('#mapComponent').exists()).toBeFalsy();
+});
+
+test('gmap Auto complete does not exist at default', () => {
+    expect(mapViewer.find('#gmapAutoComplete').exists()).toBeFalsy();
+});
+
+test('Add marker button does not exist at default', () => {
+    expect(mapViewer.find('#addMarkerButton').exists()).toBeFalsy();
+});
+
+test('Hide map button does not exist at default', () => {
+    expect(mapViewer.find('#hideMapButton').exists()).toBeFalsy();
+});
+
+test('Show map button exists at default', () => {
+    expect(mapViewer.find('#showMapButton').exists()).toBeTruthy();
+});
