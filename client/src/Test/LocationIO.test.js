cf9505ff3d56ec60338d814266fea52210f059f6
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-09-11 19:44:00 +1200

    Wrote tests for LocationIO.  #story[419]

--- client/src/Test/LocationIO.test.js
+++ client/src/Test/LocationIO.test.js
@@ -44,3 +44,26 @@ test('Hide map button does not exist at default', () => {
 test('Show map button exists at default', () => {
     expect(locationIO.find('#showMapButton').exists()).toBeTruthy();
 });
+
+test('addMarker converts a place object into a pin object', () => {
+    const autocompletePlace = {formatted_address: "Somewhere", geometry: {location: {lat: () => 0, lng: () => 0}}};
+    locationIO.vm.addMarker(autocompletePlace);
+    expect(locationIO.vm.pins.length).toBe(1);
+    expect(locationIO.vm.pins[0]).toEqual({lat:0, lng:0})
+    expect(locationIO.vm.address).toEqual("Somewhere")
+});
+
+test('addMarker converts the center of the map to a pin', () => {
+    locationIO.vm.$refs.mapViewerRef = {currentCenter: {lat:0, lng:0}};
+    locationIO.vm.addMarker();
+    expect(locationIO.vm.pins.length).toBe(1);
+    expect(locationIO.vm.pins[0]).toEqual({lat:0, lng:0})
+});
+
+test('setInputBoxUpdatePins re-positions a pin', () => {
+    const origionalAddress = locationIO.vm.address;
+    locationIO.vm.pins.push({lat:0, lng:0});
+    locationIO.vm.setInputBoxUpdatePins({lat:1, lng:1}, 0);
+    expect(locationIO.vm.pins[0]).toEqual({lat:1, lng:1});
+    expect(locationIO.vm.address).not.toEqual(origionalAddress);
+});
