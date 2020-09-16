37475e049641ed941c73e671f7cdbfe3ce714461
Ethan Burnett <ebb35@uclive.ac.nz>
2020-09-17 01:31:14 +1200

    Wrote Jest tests for `AllActivities.test.js`. Also fixed failing tests within `LocationIO.test.js` and `AllActivities.test.js`. #story[450]

--- client/src/Test/AllActivities.test.js
+++ client/src/Test/AllActivities.test.js
@@ -3,7 +3,9 @@ import {createLocalVue, shallowMount} from "@vue/test-utils";
 import {BootstrapVue} from "bootstrap-vue";
 import AllActivities from "../views/Activities/AllActivities";
 import router from "../index";
+import api from "../Api";
 
+jest.mock("../Api")
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
@@ -11,13 +13,80 @@ localVue.use(BootstrapVue);
 let allActivities;
 let config;
 
+const PINS_PER_BLOCK = 2;
+const USER_ID = 1;
+const ACTIVITY_ID_1 = 21;
+const ACTIVITY_ID_2 = 22;
+const ACTIVITY_ID_3 = 23;
+const ACTIVITY_ID_4 = 24;
+const ACTIVITY_ID_5 = 25;
+const PINS = [
+    {
+        lat: 1.0,
+        lng: -1.0,
+        colour: 'red',
+        pin_type: 'USER',
+        id: USER_ID
+    },
+    {
+        lat: 1.0,
+        lng: -1.0,
+        colour: 'blue',
+        pin_type: 'ACTIVITY',
+        id: ACTIVITY_ID_1
+    },
+    {
+        lat: 1.0,
+        lng: -1.0,
+        colour: 'blue',
+        pin_type: 'ACTIVITY',
+        id: ACTIVITY_ID_2
+    },
+    {
+        lat: 1.0,
+        lng: -1.0,
+        colour: 'blue',
+        pin_type: 'ACTIVITY',
+        id: ACTIVITY_ID_3
+    },
+    {
+        lat: 1.0,
+        lng: -1.0,
+        colour: 'blue',
+        pin_type: 'ACTIVITY',
+        id: ACTIVITY_ID_4
+    },
+    {
+        lat: 1.0,
+        lng: -1.0,
+        colour: 'green',
+        pin_type: 'ACTIVITY',
+        id: ACTIVITY_ID_5
+    }
+];
+
 beforeAll(() => {
+    api.getUserId.mockImplementation(() => {
+        return Promise.resolve({data: USER_ID, status: 200});
+    });
+    api.getActivityPins.mockImplementation((userId, blockNumber) => {
+        if (userId === USER_ID) {
+            let pins = [];
+            if (blockNumber === 0) {
+                pins.push(PINS[0]) // First block, so include the user
+            }
+            let leftIndex = blockNumber * PINS_PER_BLOCK + 1;
+            let rightIndex = leftIndex + PINS_PER_BLOCK;
+            pins = pins.concat(PINS.slice(leftIndex, rightIndex))
+            return Promise.resolve({data: pins, status: 200, headers: {'total-rows': PINS.length}});
+        }
+    });
     config = {
         router,
         localVue,
-        data: function() {
+        data() {
             return {
-                userId : 1
+                pinsPerBlock: PINS_PER_BLOCK
             }
         }
     };
@@ -28,18 +97,22 @@ test('Is a vue instance', () => {
     expect(allActivities.isVueInstance).toBeTruthy();
 });
 
-test('map pane exists on all activities page', () => {
+test('Map pane exists on all activities page', () => {
     expect(allActivities.find('#mapComponent').exists()).toBeTruthy();
 });
 
-test('activity list exists on all activities page', () => {
+test('Activity list exists on all activities page', () => {
     expect(allActivities.find('#activityList').exists()).toBeTruthy();
 });
 
-test('create new activity button exists on all activities page', () => {
+test('Create new activity button exists on all activities page', () => {
     expect(allActivities.find('#creatActivityButton').exists()).toBeTruthy();
 });
 
-test('header exists on all activities page', () => {
+test('Header exists on all activities page', () => {
     expect(allActivities.find('#header').exists()).toBeTruthy();
+});
+
+test.each(PINS)('Receive pin %o', (pin) => {
+    expect(allActivities.vm.$data.pins).toContain(pin);
 });
