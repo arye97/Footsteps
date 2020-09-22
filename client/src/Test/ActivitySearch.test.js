76071db4d0a9ec4ae67a19a23408e6b13a66414a
Saman Ighani <sig16@uclive.ac.nz>
2020-09-22 14:46:01 +1200

    Pair programming to create tests for location io searching in activity search page, took some time to diagnose errors. #story[451] #pair[sig16, rmc209]

--- client/src/Test/ActivitySearch.test.js
+++ client/src/Test/ActivitySearch.test.js
@@ -1,22 +1,52 @@
 import "vue-jest"
 import api from '../Api'
-import {shallowMount, createLocalVue} from "@vue/test-utils";
+import {shallowMount, createLocalVue, mount} from "@vue/test-utils";
 import router from "../index";
 
 import ActivitySearch from "../views/Search/ActivitySearch";
 import "jest";
 import BootstrapVue from "bootstrap-vue";
+import {gmapApi} from "gmap-vue";
+import LocationIO from "../components/Map/LocationIO";
+
+jest.mock('../Api');
+jest.mock("gmap-vue");
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
-
-jest.mock('../Api');
+localVue.use(gmapApi);
 
 const ACTIVITY_TYPES = [
     "Biking", "Hiking", "Athletics"
 ];
 
 let pageSize = 5;
+
+const parentCenterData = {
+    lng: 100.0,
+    lat: 100.0,
+    name: "ll",
+    draggable: false,
+};
+
+const parentPinData = {
+    lng: 0.0,
+    lat: 0.0,
+    name: "Activity location"
+};
+
+const userData = {
+    "private_location": {
+        "name": "private_location",
+        "lng": 10.0,
+        "lat": 10.0,
+    },
+    "public_location": {
+        "name": "public_location",
+        "lng": 15.0,
+        "lat": 15.0,
+    }
+}
 
 const SEARCH_RESPONSE1 = [
     {
@@ -109,6 +139,7 @@ beforeEach(() => {
         })
     );
 
+    api.getAllUserData.mockImplementation(() => Promise.resolve({data: userData, status: 200}))
     api.getUserId.mockImplementation(() => Promise.resolve({status: 200}));
 
     api.logout.mockImplementation(() => Promise.resolve({status:200}));
@@ -253,4 +284,30 @@ describe("Search for activities by activity type", () => {
             expect(activitySearch.vm.api.getActivityByActivityType).toHaveBeenCalledWith(["Archery","Climbing"],"and", activitySearch.vm.$data.currentPage - 1);
         });
     });
+});
+
+
+test('map pane does not exist at default', () => {
+    expect(activitySearch.find('#mapComponent').exists()).toBeFalsy();
+});
+
+test('gmap Auto complete does not exist at default', () => {
+    expect(activitySearch.find('#gmapAutoComplete').exists()).toBeFalsy();
+});
+
+test('Add marker button does not exist at default', () => {
+    expect(activitySearch.find('#addMarkerButton').exists()).toBeFalsy();
+});
+
+test('Pins are empty by default', () => {
+    api.getAllUserData.mockImplementation(() => Promise.resolve({status:200}));
+    activitySearch = shallowMount(ActivitySearch, {localVue, mocks: {api}});
+    expect(activitySearch.vm.$data.pins).toHaveLength(0);
+});
+
+test('Map center is undefined by default', () => {
+    api.getAllUserData.mockImplementation(() => Promise.resolve({status:200}));
+    activitySearch = shallowMount(ActivitySearch, {localVue, mocks: {api}});
+    expect(activitySearch.vm.$data.currentLocation.lat).toBeUndefined();
+    expect(activitySearch.vm.$data.currentLocation.lng).toBeUndefined();
 });
