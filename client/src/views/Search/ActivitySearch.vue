ab606314daea5e9eac91a9c831b640bff0464202
Rebekah McKinnon <rmc209@uclive.ac.nz>
2020-09-23 00:27:50 +1200

    Add jest tests, required adding some ids #story[466]

--- client/src/views/Search/ActivitySearch.vue
+++ client/src/views/Search/ActivitySearch.vue
@@ -38,14 +38,14 @@
                     </b-row>
                 </div>
                 <section v-if="filterSearch">
-                    <b-button size="sm" variant="link" align-self="end" v-on:click="filterSearch=false">Clear Filters</b-button><br/>
+                    <b-button id="clearFiltersButton" size="sm" variant="link" align-self="end" v-on:click="filterSearch=false">Clear Filters</b-button><br/>
                     <label>Minimum Fitness Level</label>
                     <b-form-input id="minimumFitnessLevel" type="range" min="0" max="4" focus :value="minFitness"></b-form-input>
                     <label>Maximum Fitness Level</label>
                     <b-form-input id="maximumFitnessLevel" type="range" min="0" max="4" focus :value="maxFitness"></b-form-input>
                 </section>
                 <section v-else>
-                    <b-button size="sm" variant="link" align-self="end" v-on:click="filterSearch=true">Filter Search</b-button><br/>
+                    <b-button id="filterSearchButton" size="sm" variant="link" align-self="end" v-on:click="filterSearch=true">Filter Search</b-button><br/>
                 </section>
                 <b-row>
                     <b-button class="searchButton" id="searchButton" variant="primary" v-on:click="search()">
@@ -120,7 +120,7 @@ export default {
             loading: false,
             rows: null,
             resultsFound: false,
-            filterSearch: true,
+            filterSearch: false,
             minFitness: "0",
             maxFitness: "4"
         }
