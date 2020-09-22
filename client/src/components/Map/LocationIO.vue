6ba9d9a9c1927f5c05cc0cc6c84e1438766f5be7
Ryan Akers <rak80@uclive.ac.nz>
2020-09-22 14:57:41 +1200

    Changed the "Clear pins" button to be an icon & moved it to be beside the "Drop pin" button  #story[419]

--- client/src/components/Map/LocationIO.vue
+++ client/src/components/Map/LocationIO.vue
@@ -2,10 +2,8 @@
     <div>
         <b-card class="flex-fill" border-variant="secondary">
             <div v-if="isMapVisible">
-                <b-button v-if="!viewOnly" id="clearPinsButton" variant="info" @click="clearPins" v-bind:disabled="pins.length === 0">
-                    {{singleOnly || maxPins === 1 ? "Clear Location" : "Clear Pins"}}
-                </b-button>
                 <b-button id="hideMapButton" variant="info" @click="isMapVisible=false">Hide Map</b-button>
+
                 <div v-if="!viewOnly">
                     <br/>
                 </div>
@@ -32,7 +30,12 @@
                             class="form-control" style="width: 100%">
                     </gmap-autocomplete>
                     <br/>
+                  <b-button-group id="mapButtons">
                     <b-button id='addMarkerButton' variant="primary" block @click="addMarker()">Drop Pin</b-button>
+                  <b-button v-if="!viewOnly" id="clearPinsButton" variant="danger" @click="clearPins" v-bind:disabled="pins.length === 0">
+                    <b-icon-trash-fill />
+                  </b-button>
+                  </b-button-group>
                 </div>
             </div>
             <div v-else>
