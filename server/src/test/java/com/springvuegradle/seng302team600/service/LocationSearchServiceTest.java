8f77c6d961ecdc35cd8ecf0bfff6558d6b3a65aa
Taran Jennison <tmj49@uclive.ac.nz>
2020-09-23 12:16:34 +1200

    Finished mocking repos $story[451]

--- server/src/test/java/com/springvuegradle/seng302team600/service/LocationSearchServiceTest.java
+++ server/src/test/java/com/springvuegradle/seng302team600/service/LocationSearchServiceTest.java
@@ -7,17 +7,15 @@ import com.springvuegradle.seng302team600.repository.ActivityRepository;
 import com.springvuegradle.seng302team600.repository.ActivityTypeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
 import static org.mockito.Mockito.when;
 
@@ -38,6 +36,7 @@ public class LocationSearchServiceTest {
 
     private final int PAGE_ONE = 0;
     private final int PAGE_TWO = 1;
+    private final int BLOCK_SIZE = 5;
 
     private final Long DUMMY_ACTIVITY_ID_1 = 1L;
     private final Long DUMMY_ACTIVITY_ID_2 = 2L;
@@ -99,8 +98,7 @@ public class LocationSearchServiceTest {
         activityTypeSet.add(dummyActivityType3);
         dummyActivity3.setActivityTypes(activityTypeSet);
 
-        //todo: Mock:
-        // activityTypeRepository.findActivityTypeIdsByNames(types)
+
         when(activityTypeRepository.findActivityTypeIdsByNames(Mockito.anyList())).thenAnswer(i -> {
             List<String> activityTypeStrings = i.getArgument(0);
             List<Long> resultList = new ArrayList<>();
@@ -118,14 +116,78 @@ public class LocationSearchServiceTest {
         // activityRepository.findAllWithinDistanceByAllActivityTypeIds(
         //                        coordinates.getLatitude(), coordinates.getLongitude(), cutoffDistance,
         //                        activityTypeIds, numActivityTypes, activitiesBlock)
+        when(activityRepository.findAllWithinDistanceByAllActivityTypeIds(Mockito.anyDouble(), Mockito.anyDouble(),
+                Mockito.anyDouble(), Mockito.anyList(), Mockito.anyInt(), Mockito.any(Pageable.class))).thenAnswer(i -> {
+            List<Long> activityTypeIds = i.getArgument(3);
+            int numActivityTypes = i.getArgument(4);
+
+            Pageable pageable = i.getArgument(5);
+            List<Activity> resultList = new ArrayList<>();
+            Set<ActivityType> activitiesTypes= new HashSet<>();
+            int matchCount = 0;
+
+            for (Activity activity : activityList) {
+                for (ActivityType activityType : activity.getActivityTypes()) {
+                    if (activityTypeIds.contains(activityType.getActivityTypeId())) {
+                        matchCount++;
+                    }
+                }
+                if (matchCount == numActivityTypes) {
+                    resultList.add(activity);
+                }
+                matchCount = 0;
+            }
+
+            int leftIndex = PAGE_ONE * BLOCK_SIZE;
+            int rightIndex = PAGE_TWO * BLOCK_SIZE + BLOCK_SIZE;
+            return new SliceImpl<>(resultList.subList(leftIndex, rightIndex), pageable, false);
+        });
 
         //todo: Mock:
         // activityRepository.findAllWithinDistanceBySomeActivityTypeIds(
         //                        coordinates.getLatitude(), coordinates.getLongitude(), cutoffDistance,
         //                        activityTypeIds, activitiesBlock);
+        when(activityRepository.findAllWithinDistanceBySomeActivityTypeIds(Mockito.anyDouble(), Mockito.anyDouble(),
+                Mockito.anyDouble(), Mockito.anyList(), Mockito.any(Pageable.class))).thenAnswer(i -> {
+            List<Long> activityTypeIds = i.getArgument(3);
+            Pageable pageable = i.getArgument(4);
+            List<Activity> resultList = new ArrayList<>();
+
+            for (Activity activity : activityList) {
+                for (ActivityType activityType : activity.getActivityTypes()) {
+                    if (activityTypeIds.contains(activityType.getActivityTypeId())) {
+                        resultList.add(activity);
+                        break;
+                    }
+                }
+            }
+
+            int leftIndex = PAGE_ONE * BLOCK_SIZE;
+            int rightIndex = PAGE_TWO * BLOCK_SIZE + BLOCK_SIZE;
+            return new SliceImpl<>(resultList.subList(leftIndex, rightIndex), pageable, false);
+        });
 
         //todo: Mock:
         // activityRepository.findAllWithinDistance(
         //                    coordinates.getLatitude(), coordinates.getLongitude(), cutoffDistance, activitiesBlock);
+        when(activityRepository.findAllWithinDistance(Mockito.anyDouble(), Mockito.anyDouble(),
+                Mockito.anyDouble(), Mockito.any(Pageable.class))).thenAnswer(i -> {
+            Pageable pageable = i.getArgument(3);
+            List<Activity> resultList = new ArrayList<>();
+
+            resultList.addAll(activityList);
+
+            int leftIndex = PAGE_ONE * BLOCK_SIZE;
+            int rightIndex = PAGE_TWO * BLOCK_SIZE + BLOCK_SIZE;
+            return new SliceImpl<>(resultList.subList(leftIndex, rightIndex), pageable, false);
+        });
     }
+
+//    ---- Tests ----
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
 }
