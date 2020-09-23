476d9db36eba3ddda848a9545f3b8b009d5e921c
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-09-23 23:21:46 +1200

    Moved duplicate error handling code into seperate method.  #story[451]

--- client/src/views/Search/ActivitySearch.vue
+++ client/src/views/Search/ActivitySearch.vue
@@ -252,25 +252,8 @@
                         this.loading = false;
                         this.resultsFound = true;
                     }).catch(error => {
-                    this.loading = false;
-                    this.errored = true;
-                    if ((error.code === "ECONNREFUSED") || (error.code === "ECONNABORTED")) {
-                        this.error_message = "Cannot connect to server - please try again later!";
-                    } else {
-                        if (error.response.status === 401) {
-                            this.error_message = "You aren't logged in! You're being redirected!";
-                            setTimeout(() => {
-                                this.logout()
-                            }, 3000)
-                        } else if (error.response.status === 400) {
-                            this.error_message = error.response.data.message;
-                        } else if (error.response.status === 404) {
-                            this.error_message = "No activities with activity names ".concat(this.activityTitle) + " have been found!"
-                        } else {
-                            this.error_message = "Something went wrong! Please try again."
-                        }
-                    }
-                });
+                        this.handleError(error, "No activities with activity names ".concat(this.activityTitle) + " have been found!");
+                    });
             },
 
             /**
@@ -286,25 +269,8 @@
                         this.loading = false;
                         this.resultsFound = true;
                     }).catch(error => {
-                    this.loading = false;
-                    this.errored = true;
-                    if ((error.code === "ECONNREFUSED") || (error.code === "ECONNABORTED")) {
-                        this.error_message = "Cannot connect to server - please try again later!";
-                    } else {
-                        if (error.response.status === 401) {
-                            this.error_message = "You aren't logged in! You're being redirected!";
-                            setTimeout(() => {
-                                this.logout()
-                            }, 3000)
-                        } else if (error.response.status === 400) {
-                            this.error_message = error.response.data.message;
-                        } else if (error.response.status === 404) {
-                            this.error_message = "No activities with activity types ".concat(this.selectedActivityTypes) + " have been found!"
-                        } else {
-                            this.error_message = "Something went wrong! Please try again."
-                        }
-                    }
-                });
+                        this.handleError(error, "No activities with activity types ".concat(this.selectedActivityTypes) + " have been found!");
+                    });
             },
 
 
@@ -327,27 +293,11 @@
 
                         this.resultsFound = true;
                     }).catch(error => {
-                    this.loading = false;
-                    this.errored = true;
-                    if ((error.code === "ECONNREFUSED") || (error.code === "ECONNABORTED")) {
-                        this.error_message = "Cannot connect to server - please try again later!";
-                    } else {
-                        if (error.response.status === 401) {
-                            this.error_message = "You aren't logged in! You're being redirected!";
-                            setTimeout(() => {
-                                this.logout()
-                            }, 3000)
-                        } else if (error.response.status === 400) {
-                            this.error_message = error.response.data.message;
-                        } else if (error.response.status === 404) {
-                            this.error_message = "No activities with activity types ".concat(this.selectedActivityTypes) + " have been found!"
-                        } else {
-                            this.error_message = "Something went wrong! Please try again."
-                        }
-                    }
-                });
+                        this.handleError(error, "No activities within distance of location ".concat(this.cutoffDistance) + " have been found!");
+                    });
             },
 
+            //ToDo write a JavaDoc
             async getActivityLocationRows() {
 
                 let coordinates = {
@@ -359,11 +309,34 @@
                 await api.getNumberOfRowsForActivityByLocation(urlCoordinates, this.activityTypesSearchedFor, this.cutoffDistance, this.searchType)
                     .then(response => {
                         this.rows = response.data;
-                    }).catch(() => {
-                        //ToDo do error handling?
+                    }).catch(error => {
+                        this.handleError(error, "No activities within distance of location ".concat(this.cutoffDistance) + " have been found!");
                     });
 
-                this.loading = false;  // ToDo put inside function
+                this.loading = false;
+            },
+
+
+            //ToDo write a JavaDoc
+            handleError(error, message404) {
+                this.loading = false;
+                this.errored = true;
+                if ((error.code === "ECONNREFUSED") || (error.code === "ECONNABORTED")) {
+                    this.error_message = "Cannot connect to server - please try again later!";
+                } else {
+                    if (error.response.status === 401) {
+                        this.error_message = "You aren't logged in! You're being redirected!";
+                        setTimeout(() => {
+                            this.logout()
+                        }, 3000)
+                    } else if (error.response.status === 400) {
+                        this.error_message = error.response.data.message;
+                    } else if (error.response.status === 404) {
+                        this.error_message = message404
+                    } else {
+                        this.error_message = "Something went wrong! Please try again."
+                    }
+                }
             },
 
 
