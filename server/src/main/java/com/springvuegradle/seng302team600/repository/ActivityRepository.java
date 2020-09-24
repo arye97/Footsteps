854574b40079d9271f919e646720c75ce523ba79
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2020-09-25 09:55:28 +1200

    Remove some sql that was causing issues when searching for an activity #story[422]

--- server/src/main/java/com/springvuegradle/seng302team600/repository/ActivityRepository.java
+++ server/src/main/java/com/springvuegradle/seng302team600/repository/ActivityRepository.java
@@ -60,8 +60,6 @@ public interface ActivityRepository extends JpaRepository<Activity, Long>, Activ
     @Query(value = "SELECT * FROM activity WHERE activity_id in ?1", nativeQuery = true)
     List<Activity> getActivitiesByIds(@Param("userIds") List<Long> activityIds);
 
-    @Query(value =
-            "SELECT * FROM activity WHERE activity_name LIKE ?1", nativeQuery = true)
     SearchResponse findAllByKeyword(@Param("keyword") List<String> keyword, int pageSize, int page);
 
     @Query(value = "SELECT a.*" +
